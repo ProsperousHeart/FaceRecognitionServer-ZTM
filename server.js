6f221524f6a1e8643ad68da8e8dabcf8dd32bcdb
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-14 14:32:24 -0600

    addition of Express.js database, update of endpoints

--- server.js
+++ server.js
@@ -1,32 +1,46 @@
+// since this was made as part of my learning during a bootcamp, code is messy & unclean
+// I decided to keep it with everything messy to be able to see what I did prior
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+
+const db = require('knex')({
+    client: 'pg',
+    connection: { // connection: process.env.PG_CONNECTION_STRING,
+        host : '127.0.0.1',
+        port : 5432,
+        user : 'sbrain',
+        password : 'Sm4rtBr41n2023!',
+        database : "smart-brain"
+    }
+    //searchPath: ['knex', 'public'],
+});
 
 const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 
-const db = {
-    users: [
-        {
-            id: '123',
-            name: 'MJ',
-            email: 'spiderman4me@gmail.com',
-            password: 'passon',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Silar',
-            email: 'TheGreat4ever@gmail.com',
-            password: 'fake',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
+// const db_hc = {
+//     users: [
+//         {
+//             id: '123',
+//             name: 'MJ',
+//             email: 'spiderman4me@gmail.com',
+//             password: 'passon',
+//             entries: 0,
+//             joined: new Date()
+//         },
+//         {
+//             id: '124',
+//             name: 'Silar',
+//             email: 'TheGreat4ever@gmail.com',
+//             password: 'fake',
+//             entries: 0,
+//             joined: new Date()
+//         }
+//     ]
+// }
 
 function getUser(id) {
     let found = false;
@@ -43,8 +57,8 @@ function getUser(id) {
 
 app.get('/', (req, res) => {
     //res.send('this is working');
-    res.send(db.users);
-    //res.json(db.users);
+    //res.send(db.users);
+    res.json('Welcome home!');
 })
 
 /*
@@ -59,64 +73,123 @@ app.get('/', (req, res) => {
 app.post('/register', (req, res) => {
     // create new profile
     const { email, name, password} = req.body // destructuring
-    bcrypt.hash(password, null, null, function(err, hash) {
-        // Store hash in your password DB.
-        console.log(hash);
-    });
-    db.users.push({
-        id: '126',
-        name: name,
-        email: email,
-        //password: password,
-        entries: 0,
-        joined: new Date()
+    //bcrypt.hash(password, null, null, function(err, hash) {
+    //    // Store hash in your password DB.
+    //    console.log(hash);
+    //});
+    //db.users.push({
+    //    id: '126',
+    //    name: name,
+    //    email: email,
+    //    //password: password,
+    //    entries: 0,
+    //    joined: new Date()
+    //})
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    email: loginEmail[0].email,
+                    name: name,
+                    joined: new Date()
+                //}).then(console.log)
+                })
+                //res.json(db_hc.users[db_hc.users.length - 1]);
+                .then(user => {
+                    res.status(200).json(user[0]);
+                })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
     })
-    res.json(db.users[db.users.length - 1]);
+    .catch(err => res.status(400).json('unable to register'))
 })
 
 app.post('/signin', (req, res) => {
     // check input with current list of users & if passwords match
+    
     // Load hash from your password DB.
-    //bcrypt.compare("bacon", hash, function(err, res) {
-    //    // res == true
-    //});
-    if (req.body.email === db.users[0].email &&
-        req.body.password === db.users[0].password) {
-            //res.json('success');
-            res.status(200).json(db.users[0]);
-    } else {
-        res.status(400).json('Error logging in');
-    }
+
+    // if (req.body.email === db.users[0].email &&
+    //     req.body.password === db.users[0].password) {
+    //         //res.json('success');
+    //         res.status(200).json(db.users[0]);
+    // } else {
+    //     res.status(400).json('Error logging in');
+    // }
+    //console.log(req.body);
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            //console.log(data[0]);
+            //bcrypt.compare(req.body.password, data[0], function(err, res) {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        res.status(200).json(user[0])
+                    })
+                    .catch(err => res.status(400).json('unable to get user'))
+            }
+            res.status(400).json('unable to sign in - wrong credentials')
+        })
+        .catch(err => res.status(400).json('unable to sign in'))
 })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    db.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
-        }
-    })
-    if (!found) {
-        res.status(400).json('not found');
-    }
+    //let found = false;
+    //db.users.forEach(user => {
+    //    if (user.id === id) {
+    //        found = true;
+    //        return res.json(user);
+    //    }
+    //})
+    db.select('*').from('users').where({id}) // can do this because property & value are the same
+//        id: id
+//    })
+        .then(user => {
+            if (user.length) {
+                res.status(200).json(user[0]);
+            } else {
+                res.status(400).json('Error getting user.');
+            }
+        });
+//    if (!found) {
+//        res.status(400).json('not found');
+//    }
 })
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
-    //const user = getUser(id)
-    let found = false;
-    db.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
-    })
-    if (!found) {
-        res.status(400).json('not found');
-    }
+    // //const user = getUser(id)
+    // let found = false;
+    // db.users.forEach(user => {
+    //     if (user.id === id) {
+    //         found = true;
+    //         user.entries++;
+    //         return res.json(user.entries);
+    //     }
+    // })
+    // if (!found) {
+    //     res.status(400).json('not found');
+    // }
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.status(200).json(entries[0].entries)
+        })
+        .catch(err => res.status(400).json('Unable to get count'))
 })
 
 //bcrypt.compare("veggies", hash, function(err, res) {
