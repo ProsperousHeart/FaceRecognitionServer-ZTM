10d1579af0f0f288825a404dd0abbd03ef226247
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-14 19:08:06 -0600

    dependency injection moving signin to own script

---
+++ controllers/signin.js
@@ -0,0 +1,35 @@
+const handleSignIn = (req, res, db, bcrypt) => {
+    // check input with current list of users & if passwords match
+    
+    // Load hash from your password DB.
+
+    // if (req.body.email === db.users[0].email &&
+    //     req.body.password === db.users[0].password) {
+    //         //res.json('success');
+    //         res.status(200).json(db.users[0]);
+    // } else {
+    //     res.status(400).json('Error logging in');
+    // }
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+            // console.log(data[0]);
+            //bcrypt.compare(req.body.password, data[0], function(err, res) {
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            // console.log("isValid:", isValid);
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        res.status(200).json(user[0])
+                    })
+                    .catch(err => res.status(400).json('unable to get user'))
+            }
+            res.status(400).json('unable to sign in - wrong credentials')
+        })
+        .catch(err => res.status(400).json('unable to sign in'))
+}
+
+module.exports = {
+    handleSignIn: handleSignIn
+};
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 
 const register = require("./controllers/register");
+const signIn = require("./controllers/signin");
 
 const db = require('knex')({
     client: 'pg',
@@ -76,37 +77,7 @@ app.get('/', (req, res) => {
 
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) }) // dependency injuection
 
-app.post('/signin', (req, res) => {
-    // check input with current list of users & if passwords match
-    
-    // Load hash from your password DB.
-
-    // if (req.body.email === db.users[0].email &&
-    //     req.body.password === db.users[0].password) {
-    //         //res.json('success');
-    //         res.status(200).json(db.users[0]);
-    // } else {
-    //     res.status(400).json('Error logging in');
-    // }
-    db.select('email', 'hash').from('login')
-        .where('email', '=', req.body.email)
-        .then(data => {
-            // console.log(data[0]);
-            //bcrypt.compare(req.body.password, data[0], function(err, res) {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-            // console.log("isValid:", isValid);
-            if (isValid) {
-                return db.select('*').from('users')
-                    .where('email', '=', req.body.email)
-                    .then(user => {
-                        res.status(200).json(user[0])
-                    })
-                    .catch(err => res.status(400).json('unable to get user'))
-            }
-            res.status(400).json('unable to sign in - wrong credentials')
-        })
-        .catch(err => res.status(400).json('unable to sign in'))
-})
+app.post('/signin', (req, res) => { signIn.handleSignIn(req, res, db, bcrypt) })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
