8139b4e90d2adf7f8c92c7483693cb3cb94cfd13
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-14 21:20:57 -0600

    validate no null inputs + destructuring

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,6 +1,10 @@
 //const handleSignIn = (req, res, db, bcrypt) => {
 const handleSignIn = (db, bcrypt) => (req, res) => {
     // check input with current list of users & if passwords match
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json("Please provide login credentials.");
+    }
     
     // Load hash from your password DB.
 
@@ -12,15 +16,18 @@ const handleSignIn = (db, bcrypt) => (req, res) => {
     //     res.status(400).json('Error logging in');
     // }
     db.select('email', 'hash').from('login')
-        .where('email', '=', req.body.email)
+        // .where('email', '=', req.body.email)
+        .where('email', '=', email)
         .then(data => {
             // console.log(data[0]);
             //bcrypt.compare(req.body.password, data[0], function(err, res) {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            // const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            const isValid = bcrypt.compareSync(password, data[0].hash);
             // console.log("isValid:", isValid);
             if (isValid) {
                 return db.select('*').from('users')
-                    .where('email', '=', req.body.email)
+                    // .where('email', '=', req.body.email)
+                    .where('email', '=', email)
                     .then(user => {
                         res.status(200).json(user[0])
                     })
