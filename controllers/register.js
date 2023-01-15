14b2e12a8e854a2f226267f5bcf73cf3ba2fdaf0
Kassandra Keeton (the Prosperous Heart) <ProsperousHeart@users.noreply.github.com>
2023-01-14 21:14:38 -0600

    validate no null inputs

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,9 @@
 const handleRegister = (req, res, db, bcrypt) => {
     // create new profile
-    const { email, name, password} = req.body // destructuring    
+    const { email, name, password} = req.body // destructuring
+    if (!email || !name || !password) {
+        return res.status(400).json("Incorrect registration format.");
+    }
     console.log("reg body:", req.body);
     console.log("email:", email);
     console.log("password:", password);
@@ -42,7 +45,7 @@ const handleRegister = (req, res, db, bcrypt) => {
         .catch(trx.rollback)
     })
     .catch(err => {
-        console.log(err);
+        // console.log(err);
         res.status(400).json('unable to register');
     })
 }
